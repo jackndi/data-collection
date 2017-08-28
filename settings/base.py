5668bff08aea3ce73ba86d081c1c2d9c88e25b8d
jackson <jackson@saiba.co>
2017-08-28 09:53:45 +0300

    static

--- settings/base.py
+++ settings/base.py
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/duka-static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 OTP_TOTP_ISSUER = "DUKA CONNECT"
 
