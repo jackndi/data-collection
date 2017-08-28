79dfa5c9fae78a43e1da38a1e1ecb180b30bf982
jackson <jackson@saiba.co>
2017-08-28 12:06:45 +0300

    travis-ci

--- dukaconnect/urls.py
+++ dukaconnect/urls.py
@@ -35,7 +35,6 @@ urlpatterns = [
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
     url(r'^', include('collection.urls'))
-
 ]
 
 if settings.DEBUG:
